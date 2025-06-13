3b405defed56718871a390edc36ef0a58fa71d70
Emma <you@example.com>
2025-06-13 09:24:36 +0200

    Add project files.

---
+++ Garage/Garage/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    abstract class Vehicle
+    {
+        public int Wheel { get; private set; }
+        public string Color { get; private set; }
+        public string Licenseplate { get; private set; }
+
+        protected Vehicle()
+        {
+
+        }
+
+    }
+}
